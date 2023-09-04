66184b761ec86b0641411a0851a553ff8fe2e329
AkinoJoey <gakiya213@gmail.com>
2023-09-04 15:19:57 +0900

    returnEmployeeDetail(), toString(), toHtml(), toMarkdown(), toArray()を追加

--- Users/Employee.php
+++ Users/Employee.php
@@ -4,8 +4,8 @@ namespace Users;
 
 use DateTime;
 use Users\User;
-
-class Employee extends User{
+use Interfaces\FileConvertible;
+class Employee extends User implements FileConvertible{
     private string $jobTitle;
     private float $salary;
     private DateTime $startDate;
@@ -34,5 +34,51 @@ class Employee extends User{
         $this->startDate = $startDate;
         $this->awards = $awards;
 
+    }
+    
+    public function returnEmployeeDetail() : string {
+        return sprintf("Job Title: %s, Salary: %d, Start Date: %s, Awards: ",
+        $this->jobTitle,
+        $this->salary,
+        $this->startDate->format('Y-m-d'),
+        implode(", ",$this->awards)
+        );
+    }
+
+    public function toString() : string {
+        return sprintf("ID: %d, Job Title: %s, %s %s, Start Date: %s",
+        $this->id,
+        $this->jobTitle,
+        $this->firstName,
+        $this->lastName,
+        $this->startDate->format('Y-m-d')
+        );
+    }
+
+    public function toHTML() : string {
+        return sprintf("<li>ID: %d, Job Title: %s, %s %s, Start Date: %s</li>",
+        $this->id,
+        $this->jobTitle,
+        $this->firstName,
+        $this->lastName,
+        $this->startDate->format('Y-m-d')
+        );
+    }
+
+    public function toMarkdown(): string{
+        return "## ID: {$this->id}
+                - Job Title: {$this->jobTitle}
+                - Name: {$this->firstName} {$this->lastName}
+                - Start Date: {$this->startDate}";
+    }
+
+    public function toArray(): array{
+        return [
+            'id' => ${$this->id},
+            'jobTitle' => ${$this->jobTitle},
+            'firstName' => ${$this->firstName},
+            'lastName' => ${$this->lastName},
+            'startDate' => ${$this->startDate}
+        ];
     }
 }

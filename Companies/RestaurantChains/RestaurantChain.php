cb954b454d668fd741a72dbc738f81f682c93ca6
AkinoJoey <gakiya213@gmail.com>
2023-09-05 16:08:01 +0900

    displayAllRestaurantLocations(), createAllLocationsHtml(), toHtml()を作成

--- Companies/RestaurantChains/RestaurantChain.php
+++ Companies/RestaurantChains/RestaurantChain.php
@@ -19,6 +19,9 @@ class RestaurantChain extends Company{
         array $restaurantLocations, string $cuisineType, int $numberOfLocations,
         string $parentCompany
     ) {
+        parent::__construct($name, $foundingYear, $description, $website, $phone,
+        $industry, $ceo, $isPubliclyTraded, $country, $founder, $totalEmployees);
+
         $this->chainId = $chainId;
         $this->restaurantLocations = $restaurantLocations;
         $this->cuisineType = $cuisineType;
@@ -31,6 +34,45 @@ class RestaurantChain extends Company{
     }
 
     public function displayAllRestaurantLocations() : string {
-        return implode(", ",$this->restaurantLocations);
+        $allRestaurantLocations = "";
+
+        for($i = 0; $i < count($this->restaurantLocations); $i++){
+            $location = $this->restaurantLocations[$i];
+            $allRestaurantLocations .= $location->returnDetail() . "\n";
+        }
+
+        return $allRestaurantLocations;
+    }
+
+    public function createAllLocationsHtml() : string {
+        $html = "";
+        for($i = 0; $i < count($this->restaurantLocations); $i++){
+            $restaurantLocation = $this->restaurantLocations[$i];
+            $html .=  sprintf('
+            <details>
+                <summary role="button" class="secondary">%s</summary>
+                <p>Company Name: %s,  Address: %s, ZipCode: %s</p>
+                %s
+            </details>
+            ',
+            $restaurantLocation->getName(),
+            $this->parentCompany,
+            $restaurantLocation->getAddress() . ", " . $restaurantLocation-> getCity() . ", " . $restaurantLocation->getState(),
+            $restaurantLocation->getZipCode(),
+            $restaurantLocation->toHTML());
+        }
+
+        return $html;
+    }
+
+    public function toHtml(): string{
+        return sprintf('
+        <article>
+            <header>%s</header>
+            %s
+        </article>
+        ',
+        $this->name,
+        $this->createAllLocationsHtml());
     }
 }

5548674fb4b9f6e01760c83adb04469730281402
AkinoJoey <gakiya213@gmail.com>
2023-09-06 18:00:05 +0900

    フロントエンドの作成

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once 'vendor/autoload.php';
 use Helpers\RandomGenerator;
 
 // RestaurantChainsの生成
-$restaurantChains = RandomGenerator::createObjects(2,5,'Helpers\RandomGenerator::restaurantChain');
+// $restaurantChains = RandomGenerator::createObjects(2,5,'Helpers\RandomGenerator::restaurantChain');
 ?>
 
 <!DOCTYPE html>
@@ -18,13 +18,46 @@ $restaurantChains = RandomGenerator::createObjects(2,5,'Helpers\RandomGenerator:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    
     <title>Restaurant Chain Mockup</title>
 </head>
 <body>
     <main class="container">
-        <?php foreach($restaurantChains as $restaurantChain): ?>
-            <?php echo $restaurantChain->toHTML() ?>
-        <?php endforeach ?>
+        <h3 style="text-align: center;">Restaurant Chain Mockup</h3>
+        <form action="download.php" method="post">
+                <label for="numberOfEmployees"><strong>Number of Employees</strong></label>
+                <input type="number" id="numberOfEmployees" name="numberOfEmployees" min="1" max="100" value="5">
+
+                <label><strong>Salary Range</strong></label>
+                <label for="minSalary">Min:</label>
+                <input type="number" id="minSalary" name="minSalary"  value="100">
+                <label for="maxSalary">Max:</label>
+                <input type="number" id="maxSalary" name="maxSalary"  value="10000">
+            
+
+                <label><strong>Zip Code Range</strong></label>
+                <label for="startZipCode">Start:</label>
+                <input type="number" id="startZipCode" name="startZipCode"  value="00000">
+                <label for="endZipCode">End:</label>
+                <input type="number" id="endZipCode" name="endZipCode"  value="99999">
+            
+
+                <label for="format"><strong>Download Format</strong></label>
+                <select id="format" name="format">
+                    <option value="html" selected>HTML</option>
+                    <option value="markdown">Markdown</option>
+                    <option value="json">JSON</option>
+                    <option value="txt">Text</option>
+                </select>
+
+                <button type="submit">Generate</button>
+        </form>
     </main>
 </body>
 </html>
+
+<!-- •	チェーンが持つ従業員の数を選択
+•	従業員の給与範囲を選択
+•	場所の数を入力
+•	場所の郵便番号の範囲を設定
+•	生成したいファイルのタイプを選択: HTML、JSON、TXT、または MarkDown。 -->
